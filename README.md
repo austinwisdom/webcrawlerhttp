eee02ebfb846c42dbb19ca721baa1815325ce809
wisdom-austin <austin.n.wisdom@gmail.com>
2023-01-31 11:20:23 -0300

    normalizeURL and tests

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # webcrawlerhttp
 Project for the Learn HTTP Course
+
+I did this project following a boot.dev tutorial, after completing an introductory JS class. 
+This project was finished before starting my coding bootcamp.
+
+Although I followed the tutorial, I learned and was able to practice some valuable skills:
+1. Downloading and using Jest
+2. Running tests on a .test.js doc.
+3. 
---
+++ crawl.test.js
@@ -0,0 +1,36 @@
+const {normalizeURL} = require('./crawl.js')
+const {test, expect} = require('@jest/globals')
+
+test('normalizeURL strip protocol', () => {
+    const input = 'https://blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+    // I'm expecting the actual output of normalizeURL to equal the expected output that I've specified
+    // If they do equal each other, jest will mark it as a passed test
+})
+
+test('normalizeURL strip trailing slash', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+    // I'm expecting the actual output of normalizeURL to equal the expected output that I've specified
+    // If they do equal each other, jest will mark it as a passed test
+})
+
+test('normalizeURL capital', () => {
+    const input = 'https://BLOG.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+
+})
+
+test('normalizeURL different protocol', () => {
+    const input = 'http://blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+
+})
